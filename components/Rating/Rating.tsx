2b915c4802bc8648c5750e4728b96f044f65f61d
sugarcod <102928648+sugarcod@users.noreply.github.com>
2022-10-19 17:45:20 +0300

    feat: fix rating

--- components/Rating/Rating.tsx
+++ components/Rating/Rating.tsx
@@ -3,52 +3,66 @@ import styles from "./Rating.module.sass";
 import Star from "./star.svg";
 import { useEffect, useState } from "react";
 import cn from "classnames";
-import { v4 } from 'uuid';
+import { v4 } from "uuid";
 
-export const Rating = ({isEditable, rating, setRating, className, ...props}:RatingProps ) => {
-  const [starIndex, setStarIndex] = useState<number>(rating)
-  const [ratingArray, setRatingArray] = useState<JSX.Element[]>(new Array(5).fill(<></>))
-  const [isHovering, setIsHovering] = useState(false);
-  console.log("🚀 ~ file: Rating.tsx ~ line 12 ~ Rating ~ isHovering", isHovering)
+export const Rating = ({
+  isEditable,
+  rating,
+  setRating,
+  className,
+  ...props
+}: RatingProps) => {
+  const [starIndex, setStarIndex] = useState<number>(rating);
+  const [ratingArray, setRatingArray] = useState<JSX.Element[]>(
+    new Array(5).fill(<></>)
+  );
+  const [isHovering, setIsHovering] = useState<boolean>(false);
+  const [itemIndex, setItemIndex] = useState<number>(0);
+  console.log("isHovering", isHovering, itemIndex);
 
-  useEffect(()=>{
-    fillStars()
-  }, [starIndex, isHovering])
+  useEffect(() => {
+    fillStars();
+  }, [starIndex, isHovering, itemIndex]);
 
-
-  let editArray: JSX.Element[] = []
+  let editArray: JSX.Element[] = [];
 
   const fillStars = () => {
-    if(isEditable){
+    if (isEditable) {
       ratingArray.map((star, index) => {
         editArray.push(
           <Star
-          onMouseEnter={()=>{setIsHovering(true)}}
-          onMouseLeave={()=>{setIsHovering(false)}}
-          className={isHovering ? `${styles.filled}` : ''}
-          onClick={()=>{setStarIndex(index)}}
+            key={v4()}
+            onMouseEnter={() => {
+              setIsHovering(true);
+              setItemIndex(index);
+            }}
+            onMouseLeave={() => {
+              setIsHovering(false);
+              setItemIndex(0);
+            }}
+            className={
+              isHovering && itemIndex >= index ? `${styles.filled}` : ""
+            }
+            onClick={() => {
+              setStarIndex(index + 1);
+            }}
           />
-        )
-        })
-        setRatingArray(editArray)
+        );
+      });
+      setRatingArray(editArray);
     }
 
-    if(starIndex > 0){
+    if (starIndex > 0) {
       const arrayCreator = ratingArray.map((star, index) => {
-        if(starIndex > index){
-          return <Star className={styles.filled}/>
+        if (starIndex > index) {
+          return <Star key={v4()} className={styles.filled} />;
         } else {
-          return <Star />
+          return <Star key={v4()} />;
         }
-      })
-      setRatingArray(arrayCreator)
-
+      });
+      setRatingArray(arrayCreator);
     }
-}
-  
-  
-  
-  return <div className="12">
-    {ratingArray.map(s => s)}
-  </div>
-}
+  };
+
+  return <div className={styles.rat}>{ratingArray.map((s) => s)}</div>;
+};
